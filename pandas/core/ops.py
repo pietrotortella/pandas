9512805c1427a52b8cee4cbf2446c4facd29ce8a
Pietro Tortella <lafintatartaruga@gmail.com>
2018-07-24 18:11:37 +0200

    fixed #22041, try except to catch ValueError when comparing names that are tuples against name that are numpy objects

--- pandas/core/ops.py
+++ pandas/core/ops.py
@@ -75,7 +75,7 @@ def _maybe_match_name(a, b):
     """
     Try to find a name to attach to the result of an operation between
     a and b.  If only one of these has a `name` attribute, return that
-    name.  Otherwise return a consensus name if they match of None if
+    name.  Otherwise return a consensus name if they match or None if
     they have different names.
 
     Parameters
@@ -94,10 +94,13 @@ def _maybe_match_name(a, b):
     a_has = hasattr(a, 'name')
     b_has = hasattr(b, 'name')
     if a_has and b_has:
-        if a.name == b.name:
-            return a.name
-        else:
-            # TODO: what if they both have np.nan for their names?
+        try:
+            if a.name == b.name:
+                return a.name
+            else:
+                # TODO: what if they both have np.nan for their names?
+                return None
+        except ValueError:
             return None
     elif a_has:
         return a.name
